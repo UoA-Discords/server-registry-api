eb1b4a65f870471d45ae97d986f1cace48e9435d
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-02-24 15:39:31 +1300

    remove "pseudo-namespace" services

--- src/services/AuthService/index.ts
+++
@@ -1,7 +0,0 @@
-import * as sessions from './sessions';
-import * as siteToken from './siteToken';
-
-export const AuthService = {
-    ...sessions,
-    ...siteToken,
-};
--- src/services/UserService/index.ts
+++
@@ -1,9 +0,0 @@
-import * as fetching from './fetching';
-import * as permissions from './permissions';
-import * as userDiscordData from './userDiscordData';
-
-export const UserService = {
-    ...fetching,
-    ...permissions,
-    ...userDiscordData,
-};
