9fab789a36a67549a88a8895e465b3c437346031
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-02-24 15:37:44 +1300

    remove `AppModels`

--- src/types/Database/AppModels.ts
+++
@@ -1,8 +0,0 @@
-import { Collection } from 'mongodb';
-import { Server } from '../Server';
-import { User } from '../User';
-
-export interface AppModels {
-    users: Collection<User<true>>;
-    servers: Collection<Server>;
-}
