dbd3a8a9eb3d863ac991d6a03cb7a41e2c5df539
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-02-25 19:17:56 +1300

    add `mockedInviteData` object

---
+++ src/tests/mockedInviteData.ts
@@ -0,0 +1,19 @@
+import { InviteData } from '../types/Invite';
+
+export const mockedInviteData: InviteData = {
+    guild: {
+        banner: 'test server banner',
+        description: 'test server description',
+        features: [],
+        icon: 'test server icon',
+        id: 'test server id',
+        name: 'test server name',
+        nsfw_level: 0,
+        splash: 'test server splash',
+        vanity_url_code: null,
+        verification_level: 0,
+        premium_subscription_count: 0,
+    },
+    code: 'test server invite code',
+    channel: null,
+};
