ed349bd8a975e1316e2bffd6b5c61fab5afe2960
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-02-27 22:36:37 +1300

    oops

--- src/routes/miscellaneousRoutes/getRoot.ts
+++ src/routes/miscellaneousRoutes/getRoot.ts
@@ -1,4 +1,3 @@
-import { ForbiddenError } from '../../errors/ForbiddenError';
 import { AuthScopes, EndpointProvider } from '../../types/Express/EndpointProvider';
 
 export const getRoot: EndpointProvider<AuthScopes.None, void, string> = {
@@ -6,7 +5,6 @@ export const getRoot: EndpointProvider<AuthScopes.None, void, string> = {
     permissionsRequired: null,
     applyToRoute() {
         return (_req, res) => {
-            throw new ForbiddenError('amongus');
             res.status(200).send(
                 'You found the UoA Discords server registry API!<br />Having a look around? Check out the <a href="/api-docs">API documentation</a>!',
             );
