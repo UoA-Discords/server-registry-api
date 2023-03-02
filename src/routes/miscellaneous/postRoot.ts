138dea95eb891e9b8a2388d49f845a31dca1f81c
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-03-02 17:39:45 +1300

    add stats to `postRoot` response

--- src/routes/miscellaneous/postRoot.ts
+++ src/routes/miscellaneous/postRoot.ts
@@ -5,15 +5,31 @@ interface PostRootResponse {
     startTime: ISOString;
     version: string;
     receivedRequest: ISOString;
+    numUsers: number;
+    numServers: number;
 }
 
 export const postRoot: EndpointProvider<AuthScopes.None, void, PostRootResponse> = {
     auth: AuthScopes.None,
     permissionsRequired: null,
-    applyToRoute({ config }) {
+    applyToRoute({ config, serverService, userService }) {
         const { startedAt: startTime, version } = config;
-        return (_req, res) => {
-            res.status(200).json({ startTime, version, receivedRequest: new Date().toISOString() });
+        return async (_req, res, next) => {
+            try {
+                const [numServers, numUsers] = await Promise.all([
+                    serverService.getNumPublicServers(),
+                    userService.getNumUsers(),
+                ]);
+                res.status(200).json({
+                    startTime,
+                    version,
+                    receivedRequest: new Date().toISOString(),
+                    numServers,
+                    numUsers,
+                });
+            } catch (error) {
+                next(error);
+            }
         };
     },
 };
