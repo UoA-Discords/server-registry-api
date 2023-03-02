402a21336c11655e6c69706a9b8734efb6c20ae6
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-03-02 17:11:10 +1300

    default `minServerSize` 100 -> 80

--- src/defaults/defaultConfig.ts
+++ src/defaults/defaultConfig.ts
@@ -34,7 +34,7 @@ export const defaultConfig: Config = {
 
     maxLogSize: 50,
 
-    minServerSize: 100,
+    minServerSize: 80,
 
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     version: process.env.NPM_VERSION || require('../../package.json').version,
